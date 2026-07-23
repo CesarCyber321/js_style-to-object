bf48fa70ca2814aa6fae93488aa6abde984fe39d
cesar <cesarfranca1000@gmail.com>
2026-07-22 21:20:51 -0300

    primeira submit

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const keyValuePairs = sourceString.split(';');
+
+  for (const item of keyValuePairs) {
+    if (!item.trim()) continue;
+    const parts = item.split(':');
+    const key = parts[0].trim();
+    const value = parts[1].trim();
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
